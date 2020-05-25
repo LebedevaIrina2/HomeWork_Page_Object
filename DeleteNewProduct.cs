2d923c0e704fe8ba9251b83fad793da0b39c8e21
Irina <shine_202@mail.ru>
2020-05-25 12:00:16 +0300

    add

--- DeleteNewProduct.cs
+++ DeleteNewProduct.cs
@@ -17,17 +17,13 @@ namespace PageObjects
         private IWebElement AllProductsPage => driver.FindElement(By.XPath("//a[contains(text(), 'All Products')]")); // ссылка "All Products"
 
         //private IWebElement removeNewProduct => driver.FindElement(By.XPath("(//td/a[text()=\"King prawns\"]/..//following-sibling::td[10])"));
-        private IWebElement NewProductName => driver.FindElement(By.CssSelector("King prawns"));
+        private IWebElement NewProductName => driver.FindElement(By.XPath("//td/a[text()=\"King prawns\"]"));
 
         public void RemoveProducts()
         {
             new Actions(driver).Click(AllProductsPage).SendKeys(Keys.Enter).Build().Perform();           
             new Actions(driver).Click(NewProductName).SendKeys(Keys.Tab + Keys.Tab + Keys.Enter).Build().Perform(); // Кликаем по кнопке "remove"
             driver.SwitchTo().Alert().Accept(); //Подтверждаем удаление в всплывающем окне предупреждения       
-
-            
-
-
 
         }
 
